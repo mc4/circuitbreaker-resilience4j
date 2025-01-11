fa84f8e99194d620cc75dcf12917e885c2c49f79
mc4 <mark.e.conley@gmail.com>
2025-01-11 16:25:37 -0500

    circuit breaker implementated and exception handling

--- src/main/java/com/optimism/circuitbreaker/controller/ActivityController.java
+++ src/main/java/com/optimism/circuitbreaker/controller/ActivityController.java
@@ -2,13 +2,16 @@ package com.optimism.circuitbreaker.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import com.optimism.circuitbreaker.model.Activity;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -26,11 +29,18 @@ public class ActivityController {
 	}
 
 	@GetMapping
+	@ExceptionHandler({ HttpClientErrorException.TooManyRequests.class })
+	@CircuitBreaker(name = "randomActivity", fallbackMethod = "fallback")
 	public String getRandomActivity() {
 		final ResponseEntity<Activity> responseEntity = restTemplate.getForEntity(BORED_API, Activity.class);
 		final Activity activity = responseEntity.getBody();
 		log.info("Activity: " + activity.toString());
 		return activity.getActivity();
+	}
+
+	public String fallback(final Throwable throwable) {
+		log.info("Fall back for too many requests... msg=" + throwable.toString());
+		return "Fall back for too many requests";
 	}
 
 }
